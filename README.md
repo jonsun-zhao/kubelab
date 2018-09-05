1145ee6a19f4613bf70c19264fac656a5672aeee
Neil <nmiu@google.com>
2018-09-06 00:53:05 +1000

    update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This is a living repo that _supposely_ grows with the K8s releases.
 * `apps`
   > apps built to be used as workloads or clients
 
-* `examples`
+* [examples](examples.md)
   > examples to showcase k8s features and usecases
   * `/\d+\.[\S-]+/` - GA examples
   * `staging` - work-in-progress
---
+++ tree.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+
+@md = []
+
+def directory_hash(path, name=nil)
+  data = {:data => (name || path)}
+  data[:children] = children = []
+  Dir.foreach(path) do |entry|
+    # next if (entry == '..' || entry == '.')
+    next if entry =~ /^\./
+    full_path = File.join(path, entry)
+    if File.directory?(full_path)
+      children << directory_hash(full_path, entry)
+    end
+  end
+  return data
+end
+
+
+def built_list(dir, parent=nil, level=0)
+  name = dir[:data]
+  path = parent.nil? ? name : File.join(parent, name)
+
+  @md << "  " * level + "* [#{name}](#{path})"
+
+  children = dir[:children]
+  if children.length > 0
+    children.sort_by{|c| c[:data]}.each do |c|
+      built_list(c, path, level+1)
+    end
+  end
+end
+
+# pp directory_hash("examples")
+
+built_list(directory_hash("examples"))
+# pp @md
+
+content = "# All Examples\n\n" + @md.join("\n")
+File.write("examples.md", content)
